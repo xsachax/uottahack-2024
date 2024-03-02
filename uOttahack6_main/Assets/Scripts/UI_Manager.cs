802bb85d482d5800e91b9e958dbbb6541e1eb367
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-03-02 16:56:25 -0500

    fix: rig

--- uOttahack6_main/Assets/Scripts/UI_Manager.cs
+++ uOttahack6_main/Assets/Scripts/UI_Manager.cs
@@ -7,23 +7,23 @@ using UnityEngine.UI;
 public class UI_Manager : MonoBehaviour
 {
    
-   private int _roomNumber;
+    private int _roomNumber;
    
-   [SerializeField] private TextMeshProUGUI _inputText;
-   public void onCreateRoomClick()
-   {
-      Debug.Log("Create Room Clicked");
-   }
+    [SerializeField] private TextMeshProUGUI _inputText;
+    public void onCreateRoomClick()
+    {
+        Debug.Log("Create Room Clicked");
+    }
    
-   public void onInputFieldChange()
-   {
-      _roomNumber = int.Parse(_inputText.text);
-      Debug.Log(_roomNumber);
-   }
+    public void onInputFieldChange()
+    {
+        _roomNumber = int.Parse(_inputText.text);
+        Debug.Log(_roomNumber);
+    }
    
-   public void onJoinRoomClick()
-   {
-      Debug.Log("Join Room Clicked with room number: " + _roomNumber);
+    public void onJoinRoomClick()
+    {
+        Debug.Log("Join Room Clicked with room number: " + _roomNumber);
       
-   }
+    }
 }
