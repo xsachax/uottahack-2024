d1d4e7017395e1636045cebcbeffb36c52d6c902
Sacha Arseneault <sacha.arseneault@gmail.com>
2024-03-02 16:17:42 -0500

    feat: lobby and interaction canvas

---
+++ uOttahack6_main/Assets/Scripts/.idea/.idea.Scripts.dir/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+>
+GameManager.cs,c\a\caaed996be30067a7fb4379e802bf6e27d5347ab
+=
+UI_Manager.cs,c\b\cbddb6e305a74f37931db1451ed914ba4deffcbd
---
+++ uOttahack6_main/Assets/Scripts/UI_Manager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UI_Manager : MonoBehaviour
+{
+   
+   private int _roomNumber;
+   
+   [SerializeField] private TextMeshProUGUI _inputText;
+   public void onCreateRoomClick()
+   {
+      Debug.Log("Create Room Clicked");
+   }
+   
+   public void onInputFieldChange()
+   {
+      _roomNumber = int.Parse(_inputText.text);
+      Debug.Log(_roomNumber);
+   }
+   
+   public void onJoinRoomClick()
+   {
+      Debug.Log("Join Room Clicked with room number: " + _roomNumber);
+      
+   }
+}
